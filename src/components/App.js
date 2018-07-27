f2b3a18dad91223ac5d9df461003deee6f42d3bf
Ginger Williams <gingerwilliams@Gingers-MacBook-Pro.local>
2018-07-27 12:31:17 -0400

    axios request init

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import axios from "axios";
 import "../styles/style.less";
 
 class App extends React.Component {
-  render() {
-    return <p className="title">Hello, React!</p>;
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+        };
+    }
+    
+    componentDidMount() {
+        const url = "https://jsonplaceholder.typicode.com/users";
+        axios
+            .get(url)
+            .then(function(response) {
+                console.log(response);
+                console.log(response.data);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
+
+    render() {
+        
+        return (
+            <div>
+                <div></div>
+                <p className="title">Hello, React!</p>
+            </div>
+        );
+    }
 }
 
 export default App;
