af7c888a7de661bd01d0f1bf18937ea580d4a4ef
Ginger Williams <gingerwilliams@Gingers-MacBook-Pro.local>
2018-07-31 15:39:01 -0400

    polyfill update for < ie11 test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import "babel-polyfill";
+
 ReactDOM.render(
     <App />,
     document.getElementById('root')
